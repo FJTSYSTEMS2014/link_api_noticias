9d70329040e3989e82c8ff75bba6e9a98d696b10
franck2014 <fjtsystems@gmail.com>
2021-08-19 22:46:05 -0300

    cambios del dia

--- TodoEnUno.sh
+++ TodoEnUno.sh
@@ -1,10 +1,6 @@
-echo Termino la Actualizacion...
-echo "Iniamos GIT..."
 cd C:\Users\franck\Desktop\LINK_NoticiasFranckPW3\link_api_noticias
 git init
 git pull
-git status
-echo Press Enter...
 #Noticias Generales
 curl -o top-headlines-general.json  https://newsapi.org/v2/top-headlines -G \
     -d country=ar \
@@ -57,13 +53,22 @@ curl -o top-headlines-google-news-br.json  https://newsapi.org/v2/top-headlines 
     -d sources=google-news-br \
 	-d pageSize=100 \
     -d apiKey=d2358245efe14cf09b1f1e97c2e3a7f5
-	#Noticias donde indican argentina en el mundo
-curl -o top-headlines-news-argentina-world.json  https://newsapi.org/v2/everything -G \
-    -d q=argentina \
+		#Noticias destacadas google NEWS EEUU
+curl -o top-headlines-google-news-usa.json  https://newsapi.org/v2/top-headlines -G \
+    -d country=us \
 	-d pageSize=100 \
-    -d apiKey=d2358245efe14cf09b1f1e97c2e3a7f5	
+    -d apiKey=d2358245efe14cf09b1f1e97c2e3a7f5
 	
 	
+	#Noticias donde indican argentina en el mundo
+#curl -o top-headlines-news-argentina-world.json  https://newsapi.org/v2/everything -G \
+ #   -d q=argentina \
+	#-d sources=google-news-ar \
+	#-d pageSize=100 \
+    #-d apiKey=d2358245efe14cf09b1f1e97c2e3a7f5	
+	
+	
+curl  "https://gnews.io/api/v4/search?q=argentina&token=146d3ccd32418b639499a233d323fd07&lang=en" -o top-headlines-news-argentina-world.json
 	
 #Noticias Tecnologia
 curl -o top-headlines-technology.json https://newsapi.org/v2/top-headlines -G \
@@ -72,7 +77,11 @@ curl -o top-headlines-technology.json https://newsapi.org/v2/top-headlines -G \
 	-d pageSize=100 \
     -d apiKey=d2358245efe14cf09b1f1e97c2e3a7f5
 
+echo Termino la Actualizacion...
+echo "Iniamos GIT..."
 
+git status
+echo Press Enter...
 git add .
 git commit -m "cambios del dia"
 git push
